b503a5702471a09ddd0c7fd4744d0f41370aa21a
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-10-23 14:36:30 +0530

    Display Cards Done

--- dictionary.js
+++ dictionary.js
@@ -3,11 +3,51 @@ var searchButton = document.getElementById("searchButton")
 var url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 var errors={}
 var dictionaryResults=[]
-
+var backgroundColor=['#0F9D58', '#F4B400' , '#4285F4' , '#DB4437']
 function getMeaning(inputWord)
 {
 document.getElementById("searchBar").value=""
 return url+inputWord
+}
+
+function displayCards(){
+let container=document.querySelector("#cards")
+container.innerHTML=""
+let card;
+var index=0;
+dictionaryResults.forEach(dictionaryElem=>{
+    card=document.createElement("div")
+    card.setAttribute("class","card")
+    color=backgroundColor[index++]
+    card.setAttribute("style", `background-color:$(color);color:white`)
+
+    let title=document.createElement("p")
+    title.setAttribute("class", "cardTitle")
+    title.setAttribute("style", `background-color:$(color);color:white`)
+    if(dictionaryElem.word)
+    title.innerHTML=dictionaryElem.word
+
+    let phonetic=document.createElement("p")
+    phonetic.setAttribute("class", "cardBody")
+    phonetic.setAttribute("style", `background-color:$(color);color:white`)
+    if(dictionaryElem.phonetic)
+    phonetic.innerHTML=dictionaryElem.phonetic
+
+    let origin=document.createElement("p")
+    origin.setAttribute("class", "cardBody")
+    origin.setAttribute("style", `background-color:$(color);color:white`)
+    if(dictionaryElem.origin)
+    origin.innerHTML=dictionaryElem.origin
+
+    card.appendChild(title)
+    card.appendChild(phonetic)
+    card.appendChild(origin)
+
+    container.appendChild(card)
+})
+}
+function displayErrorCard(){
+
 }
 
 function search()
@@ -19,11 +59,12 @@ function search()
      if(result.title==="No Definitions Found")
      {
          errors=result;
-
+         displayErrorCard()
      }
      else
      {
          dictionaryResults=result;
+         displayCards()
      }
  })
 }

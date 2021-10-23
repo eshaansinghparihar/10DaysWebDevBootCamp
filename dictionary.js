d38868cd90f443c688b16182f19533dcea5f051e
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-10-23 12:22:39 +0530

    Dictionary API Call Done

---
+++ dictionary.js
@@ -0,0 +1,32 @@
+var searchButton = document.getElementById("searchButton")
+
+var url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+var errors={}
+var dictionaryResults=[]
+
+function getMeaning(inputWord)
+{
+document.getElementById("searchBar").value=""
+return url+inputWord
+}
+
+function search()
+{
+ var searchInput=document.getElementById("searchBar").value
+ fetch(getMeaning(searchInput))
+ .then(response=>response.json())
+ .then(result=>{
+     if(result.title==="No Definitions Found")
+     {
+         error=result;
+
+     }
+     else
+     {
+         dictionaryResults=result;
+     }
+ })
+}
+searchButton.addEventListener("click", function(){
+search()
+})

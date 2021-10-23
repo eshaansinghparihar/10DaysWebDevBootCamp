ea267c49d3edaca11c65915255113395e14284e1
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-10-23 15:17:13 +0530

    Meanings Added

--- dictionary.js
+++ dictionary.js
@@ -3,7 +3,7 @@ var searchButton = document.getElementById("searchButton")
 var url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 var errors={}
 var dictionaryResults=[]
-var backgroundColor=['#0F9D58', '#F4B400' , '#4285F4' , '#DB4437']
+var backgroundColor=['#0F9D58','#F4B400','#4285F4' ,'#DB4437']
 function getMeaning(inputWord)
 {
 document.getElementById("searchBar").value=""
@@ -12,6 +12,7 @@ return url+inputWord
 
 function displayCards(){
 let container=document.querySelector("#cards")
+console.log(container)
 container.innerHTML=""
 let card;
 var index=0;
@@ -19,32 +20,52 @@ dictionaryResults.forEach(dictionaryElem=>{
     card=document.createElement("div")
     card.setAttribute("class","card")
     color=backgroundColor[index++]
-    card.setAttribute("style", `background-color:$(color);color:white`)
+    card.setAttribute("style", `background-color:${color};color:white`)
 
     let title=document.createElement("p")
     title.setAttribute("class", "cardTitle")
-    title.setAttribute("style", `background-color:$(color);color:white`)
+    title.setAttribute("style", `background-color:${color};color:white`)
     if(dictionaryElem.word)
     title.innerHTML=dictionaryElem.word
 
     let phonetic=document.createElement("p")
     phonetic.setAttribute("class", "cardBody")
-    phonetic.setAttribute("style", `background-color:$(color);color:white`)
+    phonetic.setAttribute("style", `background-color:${color};color:white`)
     if(dictionaryElem.phonetic)
     phonetic.innerHTML=dictionaryElem.phonetic
 
     let origin=document.createElement("p")
     origin.setAttribute("class", "cardBody")
-    origin.setAttribute("style", `background-color:$(color);color:white`)
+    origin.setAttribute("style", `background-color:${color};color:white`)
     if(dictionaryElem.origin)
-    origin.innerHTML=dictionaryElem.origin
+    origin.innerHTML="origin : "+dictionaryElem.origin
 
     card.appendChild(title)
     card.appendChild(phonetic)
     card.appendChild(origin)
 
+    let meanings=document.createElement("ul")
+    card.appendChild(meanings)
+    dictionaryElem.meanings.map(meaning=>{
+        let meaningPOS=document.createElement("ol")
+        meaningPOS.setAttribute("class","cardBody")
+        meaningPOS.setAttribute("style", `background-color:${color};color:white`)
+        meaningPOS.innerHTML="part of speech : "+meaning.partOfSpeech
+        card.appendChild(meaningPOS)
+        
+        var indexMeaning=1
+        meaning.definitions.map(defination=>{
+            let definations=document.createElement("li")
+            definations.setAttribute("class","cardBody")
+            definations.setAttribute("style", `background-color:${color};color:white`)
+            definations.innerHTML=(indexMeaning++)+" . "+defination.definition
+            card.appendChild(definations)
+        })
+    })
+
     container.appendChild(card)
 })
+index=0;
 }
 function displayErrorCard(){
 
@@ -64,7 +85,7 @@ function search()
      else
      {
          dictionaryResults=result;
-         displayCards()
+         displayCards();
      }
  })
 }

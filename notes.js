eb0eec7c1e4fed9cd3f54210637a8f857c7b0a43
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-10-25 11:41:22 +0530

    Cards added Dynamically

---
+++ notes.js
@@ -0,0 +1,46 @@
+var addNoteBtn=document.getElementById("addNoteButton")
+
+var notes=[]
+var backgroundColor=['#0F9D58','#F4B400','#4285F4' ,'#DB4437']
+
+function addNote(){
+var noteTitle=document.getElementById("addTitle").value
+var noteBody=document.getElementById("addBody").value
+if(noteTitle!="" && noteBody!="")
+{
+    var note={
+        id:notes.length,
+        title:noteTitle,
+        body:noteBody
+    }
+    notes.push(note)
+    document.getElementById("addTitle").value=""
+    document.getElementById("addBody").value=""
+    note=notes[notes.length-1]
+
+    let card=document.createElement("div")
+    card.setAttribute("class", "card")
+    color=backgroundColor[note.id%4]
+    card.setAttribute("style", `background-color:${color};color:white;`)
+
+    let title=document.createElement("input")
+    title.setAttribute("class", "cardTitle")
+    title.setAttribute("style", `background-color:${color};color:white;`)
+    title.value=note.title
+
+    let cardBody=document.createElement("textarea")
+    cardBody.setAttribute("class", "cardBody")
+    cardBody.setAttribute("style", `background-color:${color};color:white;`)
+    cardBody.value=note.body
+
+    let container=document.getElementById("cards")
+    container.appendChild(card)
+
+    card.appendChild(title)
+    card.appendChild(cardBody)
+
+}
+}
+addNoteBtn.addEventListener("click", function(){
+    addNote()
+})
